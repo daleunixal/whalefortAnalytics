e429e5163cf8134d8338b3aeab755a9d5108c5f0
NIkita Zerniukov <Nikita.Zerniukov@urfu.me>
2022-04-09 19:55:45 +0500

    Ctx added

--- src/app/children/account/pages/account-layout/account-layout.web.page.ts
+++ src/app/children/account/pages/account-layout/account-layout.web.page.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { VkBehaviourService } from '../../../../services/vk-behaviour.service';
 import { Router } from '@angular/router';
+import { tuiFadeIn } from '@taiga-ui/core';
+import { tuiPure } from '@taiga-ui/cdk';
+import { from, of } from 'rxjs';
+import { concatMap, delay, repeat, startWith } from 'rxjs/operators';
 
 @Component({
     templateUrl: './account-layout.web.page.html',
     styleUrls: [
         './styles/styles.internal.scss'
-    ]
+    ],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    animations: [tuiFadeIn]
 })
 export class AccountLayoutWebPage implements OnInit{
 
     public isInitialized: boolean = false;
+
+    public isShown$ = from([false, true]).pipe(
+        concatMap(val => of(val).pipe(delay(1.5 * 450))),
+        repeat(),
+        startWith(true),
+    );
 
     constructor(
         private _vk: VkBehaviourService,
@@ -26,6 +38,14 @@ export class AccountLayoutWebPage implements OnInit{
         }
 
         this.isInitialized = true
+    }
+
+    @tuiPure
+    public getAnimation(){
+        return {
+            value: '',
+            params: 450
+        } as const
     }
 
     public loginVK(): void{
